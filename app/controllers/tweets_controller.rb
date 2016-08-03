370d3a2deb85904f07507c5cf06101e6979a7c24
Eka430 <maple.jamox@gmail.com>
2016-08-03 19:42:18 +0900

    create追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,6 +7,11 @@ class TweetsController < ApplicationController
   end
 
   def create
+    Tweet.create(name:tweet_params[:name], title: tweet_params[:title], area: tweet_params[:area],image: tweet_params[:image])
   end
+
+  private
+  def tweet_params
+    params.permit(:name, :title, :area, :image)
 
 end
